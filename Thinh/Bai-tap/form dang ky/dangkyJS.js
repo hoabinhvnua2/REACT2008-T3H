c8b833cb7b98d7087815b518d2aac6a866a90caa
= <=>
2020-10-07 16:19:48 +0700

    upload bai tap them thành phần vào list và tính tuổi

---
+++ Thinh/Bai-tap/form dang ky/dangkyJS.js
@@ -0,0 +1,16 @@
+function kiemTra(){
+    var firstName = document.getElementById('fNameInput').value;
+    var lname = document.getElementById('lNameInput');
+    var emails = document.getElementById('ipEmail');
+    var brithday = document.getElementById('sinhNhat');
+    
+    console.log(firstName);
+    if(firstName != null){
+        return false;
+    }
+    else
+    {
+        console.log("first name loi r");
+
+    }
+}
