f0925aded94d7579472fcdaa98a44c7a8f8fd209
duy thinh <thinhogg11@gmail.com>
2020-11-04 22:44:12 +0700

    upload scss

---
+++ Thinh/MyApp/my-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li className="el1">1111111111</li>
+        <li className="el2">222222222222</li>
+        <li className="el3">333333</li>
+        <li className="el4">444444444444</li>
+        <li className="el5">555555555</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
