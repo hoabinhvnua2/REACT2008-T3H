c91ac5610e3c9c4fb0776d30e64fcbf38182eebe
Tran Van Sinh <tvsinhtdt@gmail.com>
2020-11-09 21:57:24 +0700

    9/11/2020

---
+++ Sinh/feat-sinh/todo-list/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div class="container">
+      <ul>
+        <li class="item-1">1</li>
+        <li class="item-2">2</li>
+        <li class="item-3">3</li>
+        <li class="item-4">4</li>
+        <li class="item-5">5</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
